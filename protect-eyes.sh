0a59cd88953f7dbd09714bd65e1059a15291fa88
xzpeter <xzpeter@gmail.com>
2012-07-14 21:28:26 +0800

    adding live longer alerting string.

--- protect-eyes.sh
+++ protect-eyes.sh
@@ -25,6 +25,13 @@ else
     exit 1
 fi
 
+# this is the secret to live longer:
+# http://www.ted.com/talks/jane_mcgonigal_the_game_that_can_give_you_10_extra_years_of_life.html
+live_longer_str="别忘了长寿的秘诀：\n\
+1. 高举双拳5秒钟\n\
+2. 从100开始不断减n，n为当前星期数\n\
+3. 望望窗外/窗内/看看喜欢的小动物\n\
+4. 问候一下自己在乎的人\n"
 
 # this is the count of working seconds
 working_count=0
@@ -41,7 +48,7 @@ function warn_to_rest() {
 	bar_value=`expr 100 \* $count / $max_bar`
 	echo $bar_value
     done | zenity --progress --text "已经工作了 ${working_max_mins} 分钟了！该
-    休息一下了！ ${warning_length_secs} 秒后进入屏幕保护..." --auto-close --no-cancel
+    休息一下了！ ${warning_length_secs} 秒后进入屏幕保护...\n\n${live_longer_str}" --auto-close --no-cancel
     giveup=$?
     working_count=0
     if [ $giveup = "0" ]; then
