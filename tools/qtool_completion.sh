ba4c5cef234b96ab70c11da3afabe40965974d6a
Peter Xu <peterx@redhat.com>
2015-12-31 11:30:19 +0800

    qtool_completion.sh: add complete for qtool
    Signed-off-by: Peter Xu <peterx@redhat.com>

---
+++ tools/qtool_completion.sh
@@ -0,0 +1,12 @@
+_qtool_complete()
+{
+    local sub_cmd_list
+	sub_cmd_list=$(qtool | sed '1,4d; s/:.*//')
+    COMPREPLY=()
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    if [[ "$prev" == "qtool" ]]; then
+        COMPREPLY=( $(compgen -W "${sub_cmd_list}" -- ${cur} ))
+    fi
+}
+complete -F _qtool_complete qtool
