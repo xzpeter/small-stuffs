07f00ec426fe0c3f18260c8a6ff2affb9c3d9b56
Peter Xu <peterx@redhat.com>
2017-10-30 21:58:16 +0100

    rh/q, tools/q_completion.sh: add script "q"
    It was used something like a layer between QEMU and Libvirt - we can use
    "q" command to quickly boot a VM with some specific parameters. E.g.:
    
      # q iommu+e1000+hmp
    
    will boot a VM that runs a IOMMU inside guest, plugs a e1000 card and
    also provides a HMP monitor via stdio.
    
    The funny thing is that this A+B+C+... thing can really support bash
    completion.  Just have a try.

---
+++ tools/q_completion.sh
@@ -0,0 +1,21 @@
+_q_complete()
+{
+    local sub_cmd_list
+    sub_cmd_list=$(q | grep "^- " | sed 's/^- //')
+    COMPREPLY=()
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    if [[ "$prev" == "q" ]]; then
+        if [[ "$cur" == *+* ]]; then
+            # completion with existing keys
+            existing=${cur%+*}
+            new_list=$(compgen -W "${sub_cmd_list}" -- ${cur##*+})
+            new_list=$(echo "$new_list" | sed "s/^/${existing}+/")
+            COMPREPLY=( $new_list )
+        else
+            # completion for the first key
+            COMPREPLY=( $(compgen -W "${sub_cmd_list}" -- ${cur}) )
+        fi
+    fi
+}
+complete -F _q_complete q
