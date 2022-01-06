01839c6d4b87736eb553ac7787e717ec7feb2965
Peter Xu <peterx@redhat.com>
2022-01-06 15:32:21 +0800

    update scripts
    Signed-off-by: Peter Xu <peterx@redhat.com>

--- tools/huge_vm/vm.sh
+++ tools/huge_vm/vm.sh
@@ -2,17 +2,17 @@
 
 hugepage=true
 postcopy=true
+trace=false
 hugepagesize=2M
-image=~/images/default.qcow2
-bin=~/git/qemu/bin/qemu-system-x86_64
-mem=1T
+image=~/remote/images/default.qcow2
+bin=/usr/local/bin/qemu-system-x86_64
+mem=20G
 
 param=""
 if [[ -n "$1" ]]; then
 	param="-incoming tcp:0.0.0.0:12345"
-	#param="-incoming unix:/tmp/peter.sock"
-	port=5556
-	mon=6667
+	port=5555
+	mon=6666
 else
 	port=5555
 	mon=6666
@@ -21,7 +21,7 @@ fi
 if $hugepage; then
 	param="$param -object memory-backend-memfd,id=mem,size=${mem},hugetlb=on,hugetlbsize=${hugepagesize}"
 	param="$param -numa node,memdev=mem"
-	#param="$param -mem-prealloc"
+	param="$param -mem-prealloc"
 fi
 
 if $postcopy; then
@@ -32,11 +32,17 @@ else
 	param="$param -global migration.multifd-channels=8"
 fi
 
-sudo $bin -M q35,accel=kvm -smp 128 -m ${mem} -msg timestamp=on \
+if $trace; then
+	param="$param -trace events=qemu.tracepoints"
+fi
+
+sudo $bin -M q35,accel=kvm -smp 40 -m ${mem} -msg timestamp=on \
      -name peter-vm,debug-threads=on \
      -global migration.x-max-bandwidth=0 \
-     -trace enable=runstate_set \
+     -qmp unix:/tmp/peter.qmp,server,nowait \
      -nographic -monitor telnet::${mon},server,nowait \
      -netdev user,id=net0,hostfwd=tcp::${port}-:22 \
      -device virtio-net-pci,netdev=net0 \
      $param $image
+
+cat /sys/kernel/mm/hugepages/hugepages-1048576kB/free_hugepages
